3bc350df27aeb5d6923a15a1589f982ea85e3179
fossisawesome-macbook-with-linux <fossisawesome@github.com>
2026-07-05 19:02:33 -0400

    feat: wire NwsWeatherApi into the DI container

--- app/src/main/java/com/fossisawesome/ventus/VentusApplication.kt
+++ app/src/main/java/com/fossisawesome/ventus/VentusApplication.kt
@@ -1,6 +1,7 @@
 package com.fossisawesome.ventus
 
 import android.app.Application
+import com.fossisawesome.ventus.data.api.NwsWeatherApi
 import com.fossisawesome.ventus.data.api.OpenMeteoAirQualityApi
 import com.fossisawesome.ventus.data.api.OpenMeteoGeocodingApi
 import com.fossisawesome.ventus.data.api.OpenMeteoWeatherApi
@@ -12,8 +13,9 @@ import com.fossisawesome.ventus.data.storage.AppPreferences
 class VentusApplication : Application() {
     val prefs by lazy { AppPreferences(this) }
     val weatherApi by lazy { OpenMeteoWeatherApi() }
+    val nwsWeatherApi by lazy { NwsWeatherApi() }
     val airQualityApi by lazy { OpenMeteoAirQualityApi() }
     val geocodingApi by lazy { OpenMeteoGeocodingApi() }
-    val weatherRepository by lazy { WeatherRepository(weatherApi, airQualityApi, prefs) }
+    val weatherRepository by lazy { WeatherRepository(weatherApi, nwsWeatherApi, airQualityApi, prefs) }
     val locationSource by lazy { FusedLocationSource(this) }
 }
