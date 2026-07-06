df1dd3e0c3886fd480c4acd2197f1da294cc04e9
fossisawesome-macbook-with-linux <fossisawesome@github.com>
2026-07-06 16:00:00 -0400

    chore: bump to v0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.fossisawesome.ventus"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "0.3.0"
+        versionCode = 5
+        versionName = "0.4.0"
     }
 
     signingConfigs {
